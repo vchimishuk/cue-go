31acb9da8234001b4ae3964e0c4091f47db586ec
Ant Zucaro <azucaro@gmail.com>
2015-07-21 21:14:36 -0400

    Go1 compat: err.String() -> err.Error() for tests

--- cue_test.go
+++ cue_test.go
@@ -1,9 +1,9 @@
 package cue
 
 import (
-	"testing"
-	"os"
 	"fmt"
+	"os"
+	"testing"
 )
 
 func TestPackage(t *testing.T) {
@@ -11,12 +11,12 @@ func TestPackage(t *testing.T) {
 
 	file, err := os.Open(filename)
 	if err != nil {
-		t.Fatalf("Failed to open file. %s", err.String())
+		t.Fatalf("Failed to open file. %s", err.Error())
 	}
 
 	sheet, err := Parse(file)
 	if err != nil {
-		t.Fatalf("Failed to parse file. %s", err.String())
+		t.Fatalf("Failed to parse file. %s", err.Error())
 	}
 
 	fmt.Printf("Sheet: %V\n", sheet)
